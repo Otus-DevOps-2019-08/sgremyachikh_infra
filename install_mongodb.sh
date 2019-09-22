421fee06cee043ab49fba307b816e2bda3aef738
Svetozar Gremyachikh <svetozar.gremyachih@bsc-ideas.com>
2019-09-22 16:52:57 +0300

    fixed install mongo.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
