e6372184565468d5f8fca9b8d0dc66f4e44ffedb
Svetozar Gremyachikh <svetozar.gremyachih@bsc-ideas.com>
2019-09-22 18:05:15 +0300

    startupscript fixed again3

--- startupscript.sh
+++ startupscript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/Otus-DevOps-2019-08/sgremyachikh_infra/cloud-testapp/install_ruby.sh
-wget https://raw.githubusercontent.com/Otus-DevOps-2019-08/sgremyachikh_infra/blob/cloud-testapp/install_mongodb.sh
-wget https://raw.githubusercontent.com/Otus-DevOps-2019-08/sgremyachikh_infra/blob/cloud-testapp/deploy.sh
+wget https://raw.githubusercontent.com/Otus-DevOps-2019-08/sgremyachikh_infra/cloud-testapp/install_mongodb.sh
+wget https://raw.githubusercontent.com/Otus-DevOps-2019-08/sgremyachikh_infra/cloud-testapp/deploy.sh
+sudo chmod 777 ./*.sh
 ./install_ruby.sh
 ./install_mongo.sh
 ./deploy.sh
